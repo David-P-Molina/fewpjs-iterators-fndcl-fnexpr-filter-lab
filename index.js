3935ebc8c64c010873cc44da20d45b51ed48b95c
David Moina <davemolina777@gmail.com>
2021-05-18 18:42:34 -0500

    commit tree 6837d8fd89e1768836f72195ae0cfaaab9dab3ca

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const { stringContaining } = require("expect")
+
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(search => 
+    search.toLowerCase() === name.toLowerCase()
+    )};
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(search =>
+        search[0] === name[0]
+    )
+}
+function matchName(drivers, name) {
+    return drivers.filter(search =>
+        search.name === name
+        )
+}
